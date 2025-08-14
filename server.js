fac74cf4325b2e9d6e976e629bdad6f39e57850b
Alexis Bustos <alexisbustos10@yahoo.com>
2025-08-13 17:05:28 -0700

    installed multer and cloudinary; commented code

--- server.js
+++ server.js
@@ -11,20 +11,32 @@ const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/postRoutes");
 const methodOverride = require("method-override");
 
+// Use .env file in the config folder
 require("dotenv").config({ path: "./config/.env" });
 
 // Passport config
 require("./config/passport")(passport);
 
+// Connect to DB
 connectDB();
 
+// Using EJS for views
 app.set("view engine", "ejs");
+
+// Static folder
 app.use(express.static("public"));
+
+// Body parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Logging
 app.use(logger("dev"));
+
+// Use forms for put/delete methods
 app.use(methodOverride("_method"));
-// Sessions
+
+// Setup Sessions - stored in MongoDB
 app.use(
   session({
     secret: "keyboard cat", // can be anything you want
@@ -38,8 +50,10 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Use flash messages for errors, info, ect...
 app.use(flash());
 
+// Setup routes for which the server is listening
 app.use("/", mainRoutes);
 app.use("/posts", postRoutes);
 
