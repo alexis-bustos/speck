02d1972d0c4282e5c55e7f7d1a0281cd7f04b088
Alexis Bustos <alexisbustos10@yahoo.com>
2025-08-20 17:40:31 -0700

    added route to view individual comment (for future feature)

---
+++ routes/commentRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const commentController = require("../controllers/commentController");
+const { ensureAuth } = require("../middleware/auth");
+
+router.post("/createComment", commentController.createComment);
+
+// GET -- view individual comment and the comments related to it
+// router.get('/', commentController.getCommentById)
+
+// UPDATE/EDIT
+// router.get("/edit/:id", commentController.showEditForm);
+// router.put("/:id", commentController.updateComment);
+
+// // DELETE
+// router.delete("/:id", commentController.deleteComment);
+
+module.exports = router;
