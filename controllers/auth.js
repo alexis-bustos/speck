6ea9561848e6660df43581cd412ce7f8ee2a9507
Alexis Bustos <alexisbustos10@yahoo.com>
2025-08-22 15:14:24 -0700

    fix: log out via passport callback, destory session, clear cookie, redirect to /

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,17 +44,32 @@ exports.postLogin = (req, res, next) => {
   })(req, res, next);
 };
 
-exports.logout = (req, res) => {
-  req.logout(() => {
-    console.log("User has logged out.");
-  });
-  req.session.destroy((err) => {
-    if (err)
-      console.log("Error : Failed to destroy the session during logout.", err);
-    req.user = null;
-    res.redirect("/");
+exports.logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+
+    // Destroy the session in the store
+    req.session.destroy(() => {
+      // Clear the session cookie
+      res.clearCookie("connect.sid", { path: "/" });
+      console.log("User has logged out.");
+
+      // Go to index.ejs
+      return res.redirect("/");
+    });
   });
 };
+// exports.logout = (req, res) => {
+//   req.logout(() => {
+//     console.log("User has logged out.");
+//   });
+//   req.session.destroy((err) => {
+//     if (err)
+//       console.log("Error : Failed to destroy the session during logout.", err);
+//     req.user = null;
+//     res.redirect("/");
+//   });
+// };
 
 exports.getSignup = (req, res) => {
   if (req.user) {
