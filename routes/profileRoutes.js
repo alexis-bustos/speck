7efd2bee129e8e0708c09c97370c45fbbd42ce22
Alexis Bustos <alexisbustos10@yahoo.com>
2025-08-22 14:30:22 -0700

    add POST /avatar (multer + ensureAuth)

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
+const upload = require("../middleware/multer");
 const profileController = require("../controllers/profileController");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 router.get("/:userName", profileController.getProfile);
+
+router.post(
+  "/avatar",
+  ensureAuth,
+  upload.single("avatar"),
+  profileController.uploadAvatar
+);
 
 module.exports = router;
