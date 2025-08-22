24b9571df118bc08b46e644e33a7da8c0a9a9a98
Alexis Bustos <alexisbustos10@yahoo.com>
2025-08-22 14:31:12 -0700

    for profile: handle avatar upload to Cloudinary

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,6 +1,8 @@
 const User = require("../models/User");
 const Post = require("../models/Post");
 const Comment = require("../models/Comment");
+const cloudinary = require("../middleware/cloudinary");
+const fs = require("fs");
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -27,6 +29,45 @@ module.exports = {
     } catch (error) {
       console.log(error);
       res.status(500).send("Server error");
+    }
+  },
+  uploadAvatar: async (req, res) => {
+    try {
+      if (!req.file) return res.redirect("back");
+
+      // Upload to Cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "speck/avatars",
+        overwrite: true,
+        transformation: [
+          { width: 512, height: 512, crop: "fill", gravity: "face" }, // nice square crop
+          { quality: "auto", fetch_format: "auto" },
+        ],
+      });
+
+      // Clean temp file
+      fs.unlink(req.file.path, () => {});
+
+      // Update user
+      const userId = req.user._id || req.user._id;
+      const user = await User.findById(userId);
+
+      // Delete previous avatar
+      // if (user.avatarPublicId) {
+      //   try {
+      //     await cloudinary.uploader.destroy(user.avatarPublicId);
+      //   } catch (_) {}
+      // }
+
+      user.avatarUrl = result.secure_url;
+      user.avatarPublicId = result.public_id;
+      await user.save();
+
+      // Redirect back to their profile
+      return res.redirect(`/profile/${user.userName}`);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send("Avatar upload failed");
     }
   },
 };
