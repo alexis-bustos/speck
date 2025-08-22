830bb822a09410db6f89e960e41f540087d627ce
Alexis Bustos <alexisbustos10@yahoo.com>
2025-08-21 22:35:15 -0700

    created profile router

---
+++ routes/profileRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/auth");
+const profileController = require("../controllers/profileController");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+router.get("/:userName", profileController.getProfile);
+
+module.exports = router;
